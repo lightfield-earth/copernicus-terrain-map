4280f2e3583d73c8fbe8e5b426c5e972b63352e3
Chris Diehl <chris@lightfield.earth>
2026-04-30 21:14:25 -0600

    Removed cell in the Marimo notebook that stated the notebook needed to be run from the command line to generate the HTML. No longer necessary.

--- map_notebook_marimo.py
+++ map_notebook_marimo.py
@@ -20,21 +20,7 @@ def _():
     import leafmap.maplibregl as leafmap
     from leafmap.maplibregl import Layer
 
-    return json, leafmap, mo
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""
-    ### Generating HTML for Map Deployment
-    #### December 2025
-    At present, when executing this notebook from within the Marimo interactive notebook experience, the HTML export does not function properly. The resulting HTML only includes the base map. To properly export the HTML for deployment, execute
-    ```bash
-    python map_notebook_marimo.py
-    ```
-    at the command line.
-    """)
-    return
+    return json, leafmap
 
 
 @app.cell
